c696cb4734cfbbf140779bf56eef1b2e8d771588
chen <23054115+cwkang1998@users.noreply.github.com>
2025-03-20 15:59:13 +0800

    chore: add example service code

---
+++ src/services/job_dispatcher.rs
@@ -0,0 +1,28 @@
+use eyre::Result;
+use serde::Serialize;
+use crate::queue::{sqs_message_queue::SqsMessageQueue, message_queue::Queue};
+
+#[derive(Serialize)]
+pub struct Job {
+    pub job_id: String,
+    pub start_timestamp: i64,
+    pub end_timestamp: i64,
+}
+
+pub struct JobDispatcher {
+    queue: SqsMessageQueue,
+}
+
+impl JobDispatcher {
+    pub fn new(queue: SqsMessageQueue) -> Self {
+        Self { queue }
+    }
+
+    pub async fn dispatch_job(&self, job: Job) -> Result<String> {
+        let message_body = serde_json::to_string(&job)?;
+        self.queue.send_message(message_body).await.map_err(|e| eyre::eyre!(e))?;
+        Ok(job.job_id)
+    }
+}
+
+
